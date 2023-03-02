3ee6f976434be5788470dc9755c90ed065a21f03
yanivbd7455 <126489153+yanivbd7455@users.noreply.github.com>
2023-03-02 10:18:11 +0200

    Update LoginPage.ts

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,13 +1,8 @@
 import type { Page } from "@playwright/test";
 import {
   loginBtn,
-//   labelWithText,
-//   removeButton,
-//   inputCheckbox,
-//   linkWithText,
-//   filters,
-//   dataloopItemsLabels,
-//   dataloopItemsList,
+  userTextBox,
+  pswTextBox,
 } from "../targets/loginLocators";
 
 export class LoginPage {
@@ -22,7 +17,7 @@ export class LoginPage {
   }
 
   async clickLoginBtn() {
-    await this.page.locator('button:text("Sign Up / Login")').click();
+    await this.page.locator(loginBtn).click();
   }
 
   async insertUserName(user: string) {
@@ -35,22 +30,4 @@ export class LoginPage {
     await this.page.getByPlaceholder('your password').fill(psw);
   }
 
-//   async loginInserUser(user: string , psw: string) {
-//     // await this.page.fill(dataloopInput, user);
-//     // await this.page.press(dataloopInput, "Enter");
-
-//     await this.page.click('button:text("Sign Up / Login")')
-//     //() name: 'Sign Up / Login' }).click();
-//     await this.page.getByPlaceholder('yours@example.com').fill(user);
-//     await this.page.getByPlaceholder('your password').click();
-//     await this.page.getByPlaceholder('your password').fill(psw);
-//     await this.page.getByRole('button', { name: 'Log In', exact: true }).click();
-//     // await this.page.goto('https://console.dataloop.ai/projects');
-//     // await this.page.getByRole('button', { name: 'Go to project' }).click();
-//   }
-  
-//   async loginInserPsw(psw: string) {
-//     await this.page.fill(dataloopInput, psw);
-//     await this.page.press(dataloopInput, "Enter");
-  }
 }
