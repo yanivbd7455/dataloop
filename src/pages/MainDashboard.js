ca81d6a79729512045031be2a9b06eda50ef5b7e
yanivbd7455 <126489153+yanivbd7455@users.noreply.github.com>
2023-03-02 10:35:24 +0200

    Update MainDashboard.js

--- src/pages/MainDashboard.js
+++ src/pages/MainDashboard.js
@@ -1,1 +1,24 @@
-ff
+import type { Page } from "@playwright/test";
+import {
+  projectOverview,
+  projectDropDownList,
+  projectTextBox,
+  projectDropDownList,
+  projectFirstList,
+} from "../targets/mainDashboardLocators";
+
+export class MainDashboard {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async selectProject(projectName: string) {
+    await this.page.locator(projectOverview).click();
+    await this.page.locator(projectDropDownList).click();
+    await this.page.locator(projectTextBox).fill(projectName);
+    await this.page.locator(projectFirstList).click(); 
+  }
+
+}
