c79f7570e393808156892e02048f278292d47efd
yanivbd7455 <126489153+yanivbd7455@users.noreply.github.com>
2023-03-02 11:10:04 +0200

    Update selectDataset.test.ts

--- src/tests/selectDataset.test.ts
+++ src/tests/selectDataset.test.ts
@@ -1,1 +1,17 @@
+import { test as base, expect } from "@playwright/test";
+import { MainDashboard } from "../pages/MainDashboard";
 
+//golden data per test
+const DATASET = "ImageDataSet";
+
+
+const test = base.extend<{ mainDashboard: MainDashboard }>({
+  mainDashboard: async ({ page }, use) => {
+    const mainDashboard = new MainDashboard(page);
+  },
+});
+
+  test("Select Dataset", async ({ mainDashboard }) => {
+  await mainDashboard.selectDataSet(DATASET);
+  
+});
