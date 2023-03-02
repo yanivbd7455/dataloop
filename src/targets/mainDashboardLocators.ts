7f91501d1c934a7cfffcd3815895e61c0a1b0e7c
yanivbd7455 <126489153+yanivbd7455@users.noreply.github.com>
2023-03-02 10:45:17 +0200

    Update mainDashboardLocators.ts

--- src/targets/mainDashboardLocators.ts
+++ src/targets/mainDashboardLocators.ts
@@ -11,3 +11,7 @@ export const projectTextBox = ".projectTextBox";
 export const projectDropDownList = ".projectDropDownList";
 
 export const projectFirstList = ".projectFirstList";
+
+export const datasetSearchTextBox = ".datasetSearchTextBox";
+
+export const datasetSearchFirstList = ".datasetSearchFirstList";
