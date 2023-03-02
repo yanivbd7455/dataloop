d771438c8d3ce29846eead5ecb73c503b37c7774
yanivbd7455 <126489153+yanivbd7455@users.noreply.github.com>
2023-03-02 10:50:07 +0200

    Update login.test.ts

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -1,25 +1,21 @@
 import { test as base, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 
-const TODO_ONE = "Walk the dog";
-const TODO_TWO = "Haircut";
-const TODO_THREE = "Read a book";
+//golden data per test
+const USER = "yanivbd@hotmail.com";
+const PSW = "Trans7455port";
+
 
 const test = base.extend<{ loginPage: LoginPage }>({
   loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
-   
-//     const loginBtn = await loginPage.getByRole('button', { name: 'Sign Up / Login' });
-//     expect(loginBtn === 0).toBeTruthy();
   },
 });
 
 test("Login to DataLoop Platform", async ({ loginPage }) => {
-//   await todoPage.addTodo(TODO_ONE);
-//   await todoPage.addTodo(TODO_TWO);
-//   await todoPage.addTodo(TODO_THREE);
-
-//   const length = await todoPage.listSize();
-//   expect(length === 3).toBeTruthy();
+  await loginPage.clickLoginBtn();
+  await loginPage.insertUserName(USER)
+  await loginPage.insertPsw(PSW);
+  
 });
