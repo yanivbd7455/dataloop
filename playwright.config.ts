f610ca887c51b72f15fde07ee12f76160cb5a29e
yanivbd7455 <126489153+yanivbd7455@users.noreply.github.com>
2023-03-02 09:56:47 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,14 +14,6 @@ const config: PlaywrightTestConfig = {
       name: "Chromium",
       use: { browserName: "chromium" },
     },
-    {
-      name: "Firefox",
-      use: { browserName: "firefox" },
-    },
-    {
-      name: "WebKit",
-      use: { browserName: "webkit" },
-    },
   ],
 };
 
